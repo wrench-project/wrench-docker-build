92bdc8c771075027c6ab35b99262db4f6f77ad48
Henri Casanova <henric@hawaii.edu>
2023-04-28 15:13:49 -1000

    Retired debian-buster due to problems with libstdc++fs

--- builds/build_all_locally.sh
+++ builds/build_all_locally.sh
@@ -1,4 +1,4 @@
-IMAGES=(debian-bullseye-gcc10 debian-buster-gcc8 ubuntu-focal-clang10 ubuntu-focal-gcc9 ubuntu-jammy-clang14 ubuntu-jammy-gcc11 ubuntu-jammy-gcc11-batsched)
+IMAGES=(debian-bullseye-gcc10 ubuntu-focal-clang10 ubuntu-focal-gcc9 ubuntu-jammy-clang14 ubuntu-jammy-gcc11 ubuntu-jammy-gcc11-batsched)
 
 
 for toBuild in ${IMAGES[@]}; do
